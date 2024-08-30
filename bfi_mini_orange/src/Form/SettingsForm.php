1568340df9e556ed98965689799b49599bd8e03f
Nikos Stylianou <nikosstylianou@gmail.com>
2024-08-30 13:47:15 +0100

    Adding the BFI Mini Orange module.

---
+++ bfi_mini_orange/src/Form/SettingsForm.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\bfi_mini_orange\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure BFI SSO settings for this site.
+ */
+final class SettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return 'bfi_mini_orange_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames(): array {
+    return ['bfi_mini_orange.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $form['enable_redirect_user_login'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enable user login redirect'),
+      '#default_value' => $this->config('bfi_mini_orange.settings')->get('enable_redirect_user_login'),
+      '#description' => $this->t('Enables the redirect from the user login path to SSO.'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state): void {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $this->config('bfi_mini_orange.settings')
+      ->set('example', $form_state->getValue('enable_redirect_user_login'))
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
+
+}
